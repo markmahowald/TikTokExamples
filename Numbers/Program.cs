0a4a4c498cb5392aafbf791c0a1bdb71ca0b3af5
markmahowald <markmahowald0328@gmail.com>
2022-01-11 07:06:45 -0700

    ConvertingNumbersExample created

--- Numbers/Program.cs
+++ Numbers/Program.cs
@@ -6,10 +6,31 @@ namespace Numbers
     {
         static void Main(string[] args)
         {
-            int intExample = 5;
-            float floatExample = 5.5F;
-            double doubleExample = -5.5;
+            //byte ByteExample = 1;
+            //int IntExample = 2;
+            float FloatExample = 3.3F;
+            //double DoubleExample = -4.4;
+
+    
+            double BinaryErrorExample = (double)FloatExample;
+            int BadCastExample = (int) FloatExample;
+
+            //System.Console.WriteLine($"byteExample = {ByteExample}.");
+            //System.Console.WriteLine($"intExample = {IntExample}.");
+            System.Console.WriteLine($"floatExample = {FloatExample}.");
+            //System.Console.WriteLine($"doubleExample = {DoubleExample}");
+            System.Console.WriteLine($"binaryErrorExample = {BinaryErrorExample}.");
+            //System.Console.WriteLine($"badCastExample = {BadCastExample}.");
             
+            
+            
+            
+            
+            
+            
+            System.Console.WriteLine($"Rounding after casting ="+ 
+                $" {Math.Round(BinaryErrorExample, 2)}");
+
 
         }
     }

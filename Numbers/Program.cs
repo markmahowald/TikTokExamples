0b2f2a63127e11bb94ab72ec7ec4950f416afd19
markmahowald <markmahowald0328@gmail.com>
2022-01-10 18:18:47 -0700

    Initial Commit

---
+++ Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intExample = 5;
+            float floatExample = 5.5F;
+            double doubleExample = -5.5;
+            
+
+        }
+    }
+}

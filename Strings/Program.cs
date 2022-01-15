ff637814c29668f5abae42633c60dd6d24ec52cc
markmahowald <markmahowald0328@gmail.com>
2022-01-15 09:51:59 -0700

    String tik tok example added

---
+++ Strings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string planet = "Earth";
+            string aptNumber = "42";
+            string country = "USA";
+            string streetAddress = "1234 Test St";
+            string state = "Colorado";
+
+            System.Console.WriteLine($"{streetAddress} {aptNumber}\n{state}, {country}\n{planet}");
+        }
+    }
+}
